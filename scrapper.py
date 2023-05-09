e0d345522221a9f5e445ac8e158c2c2780ffb270
jandoniec <john@techmaxi.net>
2023-05-09 23:14:26 +0200

    26.04.2023 JD FIX

--- scrapper.py
+++ scrapper.py
@@ -14,21 +14,20 @@ def get_element(ancestor,selector=None, atribute=None, return_list=False):
         return ancestor.select_one(selector).text.strip()
     except (AttributeError,TypeError):
         return None
-selectors={
-    "opinion_id":[None, "data-entry-id"],
-    "author":["span.user-post__author-name"],
-    "reccomendation":["span.user-post__author-recomendation"],
-    'score':['span.user-post__score-count'],
-    "purchased":["div.review-pz"],
-    "published_at":["span.user-post__published > time:nth-child(1)","datetime"],
-    "purchased_at":["span.user-post__published > time:nth-child(2)","datetime"],
-    "thumbs_up":["span[id^=votes-yes]"],
-    "thumbs_down":["span[id^=votes-no]"],
-    "content":["div.user-post__text"],
-    "pros": ["div.review-feature_col:has(›div.review-feature_title--positives)>div.review-feature_item",None,True],
-    "cons": ["div.review-feature_col:has(›div.review-feature_title--negatives)>div.review-feature_item",None,True],
-        
-    }
+selectors = {
+    "opinion_id": [None, "data-entry-id"],
+    "author": ["span.user-post__author-name"],
+    "recommendation": ["span.user-post__author-recomendation > em"],
+    "stars": ["span.user-post__score-count"],
+    "purchased": ["div.review-pz"],
+    "opinion_date": ["span.user-post__published > time:nth-child(1)","datetime"],
+    "purchase_date": ["span.user-post__published > time:nth-child(2)","datetime"],
+    "useful": ["button.vote-yes","data-total-vote"],
+    "unuseful": ["button.vote-no","data-total-vote"],
+    "content": ["div.user-post__text"],
+    "cons": ["div.review-feature__title--negatives ~ div.review-feature__item", None, True],
+    "pros": ["div.review-feature__title--positives ~ div.review-feature__item", None, True],
+}
 
 
 product_code = input("Podaj kod produktu: ")
